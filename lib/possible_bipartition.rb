6da05ba76b4e92af664d2f16219f340516078361
Paige Davenport <davenport.paige@outlook.com>
2020-04-05 13:54:09 -0700

    Completed possible_bipartition method

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,39 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  return true if dislikes.length < 2
+
+  stack = []
+  visited = {}
+
+  dislikes.each_index do |i|
+    if !visited[i]
+      stack << i
+      visited[i] = "blue"
+    end
+
+    while !stack.empty?
+      curr_dog = stack.pop
+      disliked_dogs = dislikes[curr_dog]
+
+      disliked_dogs.each do |dog|
+        if !visited[dog]
+          stack << dog
+          visited[dog] = change_color(visited[curr_dog])
+        elsif visited[dog] == visited[curr_dog]
+          return false
+        end
+      end
+    end
+  
+  end
+
+  return true
+end
+
+def change_color(color)
+  if color == "blue"
+    return "green"
+  else
+    return "blue"
+  end
 end
